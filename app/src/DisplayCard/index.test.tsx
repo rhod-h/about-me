b5a94e6cd448d3b3c8cad81f27e866de6df6c34e
Rhodri Hewitson <rhodrih@amdigital.co.uk>
2022-04-10 13:19:14 +0100

    Fix permission and add test for DisplayCard

--- app/src/DisplayCard/index.test.tsx
+++ app/src/DisplayCard/index.test.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import TestHat from '../Resources/Hat.jpeg';
 
 import DisplayCard from '.';
 
-// test('shows header text on a page', () => {
-//   const testMessage = 'Test Message';
-//   render(<Header label={testMessage} />);
+test('shows DisplayCard text on a page', () => {
+  const testHeading = 'Test Heading';
+  const testDescribe = 'Test Describe';
+  const testLinkUrl = 'www.test.com';
+  const testLinkLabel = 'Test Link';
 
-//   expect(screen.getByText(testMessage)).toBeInTheDocument();
-// });
+  render(<DisplayCard
+    heading={testHeading}
+    describe={testDescribe}
+    image={TestHat}
+    url={testLinkUrl}
+    linkLabel={testLinkLabel}
+  />);
+
+  expect(screen.getByText(testHeading)).toBeInTheDocument();
+  expect(screen.getByText(testDescribe)).toBeInTheDocument();
+  expect(screen.getByText(testLinkLabel)).toBeInTheDocument();
+});
+
+// TODO test image and link?
+// create test for alt test
